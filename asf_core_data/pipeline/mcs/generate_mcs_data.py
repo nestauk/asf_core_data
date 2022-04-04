ef920aa017942acc299d932b0892db44bbc363a7
Chris Williamson <chris.williamson@nesta.org.uk>
2022-04-04 14:06:11 +0100

    editing script to generate and save MCS data

--- asf_core_data/pipeline/mcs/generate_mcs_data.py
+++ asf_core_data/pipeline/mcs/generate_mcs_data.py
@@ -8,7 +8,7 @@ from asf_core_data.pipeline.mcs.process.mcs_epc_joining import (
     join_mcs_epc_data,
     select_most_relevant_epc,
 )
-from asf_core_data.getters.data_getters import s3, save_to_s3
+from asf_core_data.getters.data_getters import s3, load_s3_data, save_to_s3
 
 config = get_yaml_config(Path(str(PROJECT_DIR) + "/asf_core_data/config/base.yaml"))
 
@@ -20,17 +20,27 @@ mcs_installations_epc_most_relevant_path = config[
     "MCS_INSTALLATIONS_EPC_MOST_RELEVANT_PATH"
 ]
 
+keyword_to_path_dict = {
+    "none": mcs_installations_path,
+    "full": mcs_installations_epc_full_path,
+    "newest": mcs_installations_epc_newest_path,
+    "most_relevant": mcs_installations_epc_most_relevant_path,
+}
 
-def generate_processed_mcs(epc_version, geocode=True):
+
+def generate_processed_mcs_installations(epc_version="none"):
 
     if epc_version not in ["none", "full", "newest", "most_relevant"]:
         raise ValueError(
             "epc_version should be one of 'none', 'full', 'newest' or 'most_relevant'"
         )
+
+    processed_mcs = get_processed_mcs_data(save=False)
+
     if epc_version == "none":
-        processed_mcs = get_processed_mcs_data(save=False)
+        return processed_mcs
     else:
-        joined_mcs_epc = join_mcs_epc_data(save=False, all_records=True)
+        joined_mcs_epc = join_mcs_epc_data(hps=processed_mcs, all_records=True)
         if epc_version == "newest":
             processed_mcs = joined_mcs_epc.loc[
                 joined_mcs_epc.groupby("index_x")["INSPECTION_DATE"].idxmax()
@@ -40,24 +50,68 @@ def generate_processed_mcs(epc_version, geocode=True):
         else:
             processed_mcs = joined_mcs_epc
 
-    if geocode:
-        print("geocoding not yet implemented")
-        # add geocoding here
-
     return processed_mcs
 
 
-if __name__ == "__main__":
-    # Get today's date for filename suffix
+def get_mcs_installations(epc_version="none", refresh=False):
+
+    if not refresh:
+        bucket = s3.Bucket(bucket_name)
+        folder = "outputs/MCS/"
+        file_list = [object.key for object in bucket.objects.filter(Prefix=folder)]
+        file_prefix = keyword_to_path_dict[epc_version]
+        matches = [
+            filename for filename in file_list if filename.startswith(file_prefix)
+        ]
+        if len(matches) > 0:
+            latest_version = max(matches) + ".csv"
+            print("Loading", latest_version, "from S3")
+            mcs_installations = load_s3_data(
+                s3, bucket_name, keyword_to_path_dict[epc_version]
+            )
+            return mcs_installations
+        else:
+            print("File not found on S3.")
+
+    print("Generating required data...")
+    mcs_installations = generate_processed_mcs_installations(epc_version=epc_version)
+
+    return mcs_installations
+
+
+def generate_and_save_mcs():
     today = date.today().strftime("%y%m%d")
+
     # Get all four versions and save them in S3
-    for parameter, path in [
-        ("none", mcs_installations_path),
-        ("full", mcs_installations_epc_full_path),
-        ("newest", mcs_installations_epc_newest_path),
-        ("most_relevant", mcs_installations_epc_most_relevant_path),
-    ]:
-        data = generate_processed_mcs(epc_version=parameter)
-        full_path = path + "_" + today + ".csv"
-        save_to_s3(s3, bucket_name, data, full_path)
-        print("Saved in S3: " + full_path)
+
+    no_epc_path, full_epc_path, newest_epc_path, most_relevant_epc_path = [
+        (path_stem + "_" + today + ".csv")
+        for path_stem in [
+            mcs_installations_path,
+            mcs_installations_epc_full_path,
+            mcs_installations_epc_newest_path,
+            mcs_installations_epc_most_relevant_path,
+        ]
+    ]
+
+    processed_mcs = get_processed_mcs_data(save=False)
+    save_to_s3(s3, bucket_name, processed_mcs, no_epc_path)
+    print("Saved in S3: " + no_epc_path)
+
+    fully_joined_mcs_epc = join_mcs_epc_data(hps=processed_mcs, all_records=True)
+    save_to_s3(s3, bucket_name, fully_joined_mcs_epc, full_epc_path)
+    print("Saved in S3: " + full_epc_path)
+
+    newest_mcs_epc = fully_joined_mcs_epc.loc[
+        fully_joined_mcs_epc.groupby("index_x")["INSPECTION_DATE"].idxmax()
+    ]
+    save_to_s3(s3, bucket_name, newest_mcs_epc, newest_epc_path)
+    print("Saved in S3: " + newest_epc_path)
+
+    most_relevant_mcs_epc = select_most_relevant_epc(fully_joined_mcs_epc)
+    save_to_s3(s3, bucket_name, most_relevant_mcs_epc, most_relevant_epc_path)
+    print("Saved in S3: " + most_relevant_mcs_epc)
+
+
+if __name__ == "__main__":
+    generate_and_save_mcs()
