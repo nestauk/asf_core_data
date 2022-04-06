892f1b57350dc39a1fb7f557656b92a4f5964cce
India Kerle <india.kerle@nesta.org.uk>
2022-04-06 18:23:24 +0100

    bash script to pull data from google drive to s3 then test latest installers and installations data

---
+++ asf_core_data/pipeline/mcs/test/mcs_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python gdrive_to_s3.py
+latest_outputs=$(aws s3api list-objects --bucket asf-core-data --prefix inputs/MCS/latest_raw_data/ --output text --query "Contents[].{Key: Key}")
+latest_outputs_array=($latest_outputs)
+new_installations="${latest_outputs_array[1]}"
+new_installers="${latest_outputs_array[2]}"
+python compare_mcs_installations.py --new_df "${new_installations}" --comp_df "${new_installers}"
