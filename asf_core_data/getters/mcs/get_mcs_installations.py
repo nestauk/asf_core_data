648eabc76a79afbeb4628888a572a619d0fb059e
Chris Williamson <chris.williamson@nesta.org.uk>
2022-05-20 15:18:53 +0100

    simplifying getter function

--- asf_core_data/getters/mcs/get_mcs_installations.py
+++ asf_core_data/getters/mcs/get_mcs_installations.py
@@ -25,8 +25,7 @@ def get_raw_installations_data(
     verbose=True,
 ):
     """Get raw MCS HP installation data (both domestic and non-domestic)
-    with no processing other than renaming columns, setting correct dtypes
-    and removing duplicate records.
+    with no processing other than renaming columns and setting correct dtypes.
 
     Args:
         local_path (str, optional): Local path to raw data.
@@ -35,48 +34,43 @@ def get_raw_installations_data(
             of the data from S3. If local_path does not exist, data will be
             pulled from S3 even if refresh is False. Defaults to True.
         verbose (bool, optional): Whether to print info about the number of
-            samples before and after deduplication.
+            samples in loaded data.
 
     Returns:
         DataFrame: Raw HP installation data.
     """
 
     if refresh or not os.path.exists(local_path):
-        data = load_s3_data(s3, BUCKET_NAME, INSTALLATIONS_RAW_S3_PATH)
-        hps = data.astype(
-            {
-                "Address Line 1": str,
-                "Address Line 2": str,
-                "Address Line 3": str,
-                "Postcode": str,
-            }
-        )
-        hps["Commissioning Date"] = hps["Commissioning Date"].dt.to_pydatetime()
-        hps = hps.rename(columns=colnames_dict)
-
+        hps = load_s3_data(s3, BUCKET_NAME, INSTALLATIONS_RAW_S3_PATH)
     else:
-        hps = (
-            pd.read_excel(
-                local_path,
-                dtype={
-                    "Commissioning Date": dt.datetime,
-                    "Address Line 1": str,
-                    "Address Line 2": str,
-                    "Address Line 3": str,
-                    "Postcode": str,
-                },
-            )
-            .rename(columns=colnames_dict)
-            .convert_dtypes()
+        hps = pd.read_excel(local_path)
+
+    hps = hps.astype(
+        {
+            "Address Line 1": str,
+            "Address Line 2": str,
+            "Address Line 3": str,
+            "Postcode": str,
+        }
+    )
+    hps["Commissioning Date"] = pd.to_datetime(hps["Commissioning Date"])
+    hps = (
+        hps.rename(columns=colnames_dict)
+        .fillna({"address_1": "", "address_2": "", "address_3": ""})
+        .drop_duplicates(
+            subset=[
+                "version",
+                "commission_date",
+                "address_1",
+                "address_2",
+                "address_3",
+                "postcode",
+            ]
         )
+        .reset_index(drop=True)
+    )
 
     if verbose:
-        print("Original # samples:", hps.shape)
-
-    hps.fillna({"address_1": "", "address_2": "", "address_3": ""}, inplace=True)
-    hps.drop_duplicates(inplace=True)
-
-    if verbose:
-        print("After dropping duplicates, # samples:", hps.shape)
+        print("Shape of loaded data:", hps.shape)
 
     return hps
