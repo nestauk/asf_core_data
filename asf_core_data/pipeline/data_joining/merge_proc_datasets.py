3053c0fbc15041c0bf07421447ce88780ff51d8f
Julia Suter <juliasuter@NESTA-FVFF81W0Q05P.local>
2023-02-25 14:14:28 +0000

    Merging EPC and MCS data in meaningful way (#39)

--- asf_core_data/pipeline/data_joining/merge_proc_datasets.py
+++ asf_core_data/pipeline/data_joining/merge_proc_datasets.py
@@ -11,6 +11,14 @@ import numpy as np
 
 
 def merge_proc_epc_and_mcs_installs(epc_df):
+    """Merge processed EPC and MCS installations data
+
+    Args:
+        epc_df (pd.DataFrame): Processed EPC dataframe.
+
+    Returns:
+        pd.DataFrame: Merged EPC and MCS installations dataframe.
+    """
 
     newest_joined_batch = data_batches.get_latest_mcs_epc_joined_batch()
 
@@ -44,28 +52,51 @@ def merge_proc_epc_and_mcs_installs(epc_df):
         format="%Y%m%d",
     )
 
-    print(mcs_df.shape)
-    print(epc_df.shape)
-
-    mcs_df["HP_INSTALLED"] = True
     mcs_df["MCS_AVAILABLE"] = True
     epc_df["EPC_AVAILABLE"] = True
     mcs_df["EPC_AVAILABLE"] = np.where(~mcs_df["UPRN"].isna(), True, False)
 
-    epc_mcs_df = pd.merge(epc_df, mcs_df, on=["UPRN"], how="outer")
+    mcs_matched_df = mcs_df[mcs_df["EPC_AVAILABLE"]]
+    mcs_non_matched_df = mcs_df[~mcs_df["EPC_AVAILABLE"]]
 
-    # , 'MCS_AVAILABLE', 'EPC_AVAILABLE', 'HP_INSTALLED'], how = 'outer')
+    # print(mcs_df["EPC_AVAILABLE"].value_counts(dropna=False))
+    # print(epc_df["MCS_AVAILABLE"].value_counts(dropna=False))
+    # print(mcs_df.shape)
+    # print(epc_df.shape)
 
-    print(epc_mcs_df.shape)
+    epc_mcs_df = pd.merge(
+        epc_df,
+        mcs_matched_df,
+        on=["UPRN", "EPC_AVAILABLE", "MCS_AVAILABLE"],
+        how="outer",
+    )
 
-    # epc_mcs_df['HP_INSTALL_DATE'] = np.where(epc_mcs_df['MCS_AVAILABLE'], epc_mcs_df['commission_date'], epc_mcs_df['HP_INSTALL_DATE'])
+    epc_mcs_df = pd.concat([epc_mcs_df, mcs_non_matched_df], axis=0)
 
-    # epc_mcs_df = standardise_hp_type(epc_mcs_df)
+    epc_mcs_df["HP_INSTALL_DATE"] = np.where(
+        epc_mcs_df["MCS_AVAILABLE"],
+        epc_mcs_df["commission_date"],
+        epc_mcs_df["HP_INSTALL_DATE"],
+    )
+
+    epc_mcs_df["HP_INSTALLED"] = np.where(
+        (epc_mcs_df["HP_INSTALLED"]) | (epc_mcs_df["MCS_AVAILABLE"]), True, False
+    )
+
+    epc_mcs_df = standardise_hp_type(epc_mcs_df)
 
     return epc_mcs_df
 
 
 def standardise_hp_type(epc_mcs_df):
+    """Standardise HP type between EPC and MCS data.
+
+    Args:
+        epc_mcs_df (pd.DataFrame): Merged EPC and MCS dataframe.
+
+    Returns:
+        pd.DataFrame: Merged EPC and MCS dataframe with standardised HP type.
+    """
 
     type_dict = {
         "No HP": np.NaN,
@@ -79,12 +110,11 @@ def standardise_hp_type(epc_mcs_df):
         "Ground/Water Source Heat Pump": "Ground/Water Source Heat Pump",
     }
 
-    print("get here")
+    # Overwrite with tech type from MCS if MCS data exists
     epc_mcs_df["HP_TYPE"] = np.where(
         epc_mcs_df["MCS_AVAILABLE"], epc_mcs_df["tech_type"], epc_mcs_df["HP_TYPE"]
     )
 
-    print("also get here")
     epc_mcs_df["HP_TYPE"] = epc_mcs_df["HP_TYPE"].map(type_dict)
 
     return epc_mcs_df
