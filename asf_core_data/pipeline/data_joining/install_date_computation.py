d8d4a893d65d7cab226fce0b739ce0a87813f6ff
sofiapinto <anasbpinto3@gmail.com>
2023-05-12 12:24:46 +0100

    changing the way we update install dates a fix typo re HP_INSTALLED

--- asf_core_data/pipeline/data_joining/install_date_computation.py
+++ asf_core_data/pipeline/data_joining/install_date_computation.py
@@ -97,6 +97,127 @@ def compute_hp_install_date(
     If there isn't an MCS installation record, but EPC mentions having a heat pump, we consider
     the inspection date of the first EPC record for said property that mentions the heat pump.
 
+    Check this sketch to understand the logic (numbers are outdated):
+    https://docs.google.com/presentation/d/1L9Xu15tjLA9qhMbUz7KYuRwJE7iLC1hBxNDhLnisPWk/edit#slide=id.g242cc1a2059_1_0
+
+    Note that this function doesn't merge EPC with MCS, it merely draws in MCS data to create a better estimate
+    of the installation date.
+
+    Args:
+        df (pd.DataFrame): Dataframe with EPC data.
+        identifier (str, optional): Unique identifier for properties. Defaults to "UPRN".
+        verbose (bool, optional): Print some diagnostics. Defaults to True.
+        add_hp_features (bool, optional): Compute additional features regarding mentions of heat pumps. Defaults to False.
+
+    Returns:
+        pd.DataFrame: EPC data with updated install dates.
+    """
+
+    # Get the MCS install dates for EPC properties
+    mcs_hp_date_dict = get_mcs_install_date_mapping()
+    df["HP_INSTALL_DATE"] = df["UPRN"].map(mcs_hp_date_dict)
+
+    df = df[df["INSPECTION_DATE"].notna()]
+
+    # Get the first heat pump mention for each property
+    first_hp_mention = (
+        df.loc[df["HP_INSTALLED"]].groupby(identifier)["INSPECTION_DATE"].min()
+    )
+
+    df["FIRST_HP_MENTION"] = df[identifier].map(dict(first_hp_mention))
+
+    # Additional features about heat pump history of property
+    # Interesting to track unexpected behaviours, e.g. having lost a heat pump
+    if add_hp_features:
+        df["HP_AT_ANY_POINT"] = df[identifier].map(
+            dict(df.groupby(identifier)["HP_INSTALLED"].max())
+        )
+
+        df["HP_AT_FIRST"] = df[identifier].map(
+            df.loc[df.groupby(identifier)["INSPECTION_DATE"].idxmin()]
+            .set_index(identifier)
+            .to_dict()["HP_INSTALLED"]
+        )
+
+        df["HP_AT_LAST"] = df[identifier].map(
+            df.loc[df.groupby(identifier)["INSPECTION_DATE"].idxmax()]
+            .set_index(identifier)
+            .to_dict()["HP_INSTALLED"]
+        )
+
+        df["HP_LOST"] = df["HP_AT_FIRST"] & ~df["HP_AT_LAST"]
+        df["HP_ADDED"] = ~df["HP_AT_FIRST"] & df["HP_AT_LAST"]
+        df["HP_IN_THE_MIDDLE"] = (
+            ~df["HP_AT_FIRST"] & ~df["HP_AT_LAST"] & ~df["FIRST_HP_MENTION"].isna()
+        )
+
+        # If no first mention of HP, then set has no heat pump
+        df["HAS_HP_AT_SOME_POINT"] = ~df["FIRST_HP_MENTION"].isna()
+
+    # If no HP Install date, set MCS availabibility to False
+    df["MCS_AVAILABLE"] = ~df["HP_INSTALL_DATE"].isna()
+
+    # HP entry conditions
+    no_mcs_or_epc = (~df["MCS_AVAILABLE"]) & (~df["HP_INSTALLED"])
+    no_mcs_but_epc_hp = (~df["MCS_AVAILABLE"]) & (df["HP_INSTALLED"])
+    mcs_and_epc_hp = (df["MCS_AVAILABLE"]) & (df["HP_INSTALLED"])
+
+    if verbose:
+        print("Total", df.shape[0])
+        print("MCS and EPC", df[mcs_and_epc_hp].shape[0])
+        print("no MCS or EPC", df[no_mcs_or_epc].shape[0])
+        print("no MCS but EPC", df[no_mcs_but_epc_hp].shape[0])
+
+    # --- Handling the various cases ---
+
+    # 1) NO MCS/EPC HP entry
+    # --> No heat pump, no install date
+    # No changes to data required.
+
+    # -----
+
+    # 2) No MCS entry but EPC HP entry
+    # --> HP: yes (already set), install date: first HP mention
+
+    df["HP_INSTALL_DATE"] = np.where(
+        no_mcs_but_epc_hp, df["FIRST_HP_MENTION"], df["HP_INSTALL_DATE"]
+    )
+
+    if not add_hp_features:
+        df.drop(columns="FIRST_HP_MENTION", inplace=True)
+
+    # -----
+
+    # 3) MCS and EPC HP entry
+    # --> HP: yes,  install date: MCS install date
+    # No changes to data required.
+
+    # --- End of handling edge cases ---
+
+    # Deduplicate and only keep latest record
+    df = df.sort_values("INSPECTION_DATE", ascending=True).drop_duplicates(
+        subset=[identifier], keep="last"
+    )
+
+    return df
+
+
+def compute_hp_install_date_detailed_edge_cases(
+    df,
+    identifier="UPRN",
+    verbose=False,
+    add_hp_features=False,
+):
+    """
+    ---This is a different version of compute_hp_install_date(), where we deal with detailed
+    edge cases!---
+
+    Compute and update the heat pump installation date based on combined information from EPC and MCS.
+    We get the best approximation for the installation date as follows:
+    If there is an MCS installation record for this property, we use the HP commissioning date.
+    If there isn't an MCS installation record, but EPC mentions having a heat pump, we consider
+    the inspection date of the first EPC record for said property that mentions the heat pump.
+
     We also handle some edge cases, for example if an EPC record doesn't mention
     a heat pump at a time where there already should have been one according to MCS records.
 
@@ -132,7 +253,6 @@ def compute_hp_install_date(
     # Additional features about heat pump history of property
     # Interesting to track unexpected behaviours, e.g. having lost a heat pump
     if add_hp_features:
-
         df["HP_AT_ANY_POINT"] = df[identifier].map(
             dict(df.groupby(identifier)["HP_INSTALLED"].max())
         )
@@ -174,7 +294,6 @@ def compute_hp_install_date(
     df["ARTIFICIALLY_DUPL"] = False
 
     if verbose:
-
         print("Total", df.shape[0])
         print("MCS and EPC", df[mcs_and_epc_hp].shape[0])
         print("no MCS or EPC", df[no_mcs_or_epc].shape[0])
@@ -228,7 +347,7 @@ def compute_hp_install_date(
 
     # -----
 
-    # MCS and EPC HP entry, EPC entry before MCS installatioon
+    # MCS and EPC HP entry, EPC entry before MCS installation
     # ! We want to discard that option as it should not happen!
     # Set HP to False and Install Date to NA
 
@@ -263,10 +382,6 @@ def compute_hp_install_date(
         (no_epc_but_mcs_hp & ~epc_entry_before_mcs),
         np.datetime64("NaT"),
         df["HP_INSTALL_DATE"],
-    )
-
-    df["HP_INSTALLED"] = np.where(
-        (no_epc_but_mcs_hp & ~epc_entry_before_mcs), True, df["HP_INSTALLED"]
     )
 
     # ---
